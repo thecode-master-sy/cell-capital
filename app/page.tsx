60b45a0f782b82cdf02fda0044d996e3f56aeccc
thecode-master-sy <thecodemasster@gmail.com>
2025-05-07 21:50:10 +0100

    our services section design

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import AboutSection from "@/components/sections/about-section";
 import CallToActionSection from "@/components/sections/call-to-action";
+import Footer from "@/components/sections/footer";
 import HeroSection from "@/components/sections/hero-section";
 import OurServices from "@/components/sections/our-services";
 import MeetOurTeam from "@/components/sections/our-team";
 import SocialProof from "@/components/sections/social-proof";
 import TestimonalSection from "@/components/sections/testimonials-section";
+
 
 export default function Home() {
 	return (
@@ -16,6 +18,7 @@ export default function Home() {
 			<OurServices />
 			<TestimonalSection />
 			<CallToActionSection />
+			<Footer />
 		</div>
 	);
 }
