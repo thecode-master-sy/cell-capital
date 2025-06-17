028e019c0a5328c1699713787ef07ab82ff455ef
thecode-master-sy <thecodemasster@gmail.com>
2025-06-17 23:49:42 +0100

    feat: consultation form design

--- components/cal-component.tsx
+++ components/cal-component.tsx
@@ -3,6 +3,7 @@ import Cal, { getCalApi } from "@calcom/embed-react";
 import { useConsultationFormContext } from "./providers/consultation-provider";
 import { useEffect, useRef } from "react";
 import { useLenis } from "lenis/react";
+import { X } from "lucide-react";
 
 type BookerProps = {};
 
@@ -61,12 +62,18 @@ export default function Booker() {
     bookerOpen && (
       <div
         ref={overlayRef}
-        className="fixed inset-0 flex justify-center z-[100] gap-4 overflow-y-auto p-4 menu-backdrop"
+        className="fixed inset-0 flex justify-center z-[100] gap-4 overflow-y-auto p-4 py-12 menu-backdrop"
       >
-        <div
-          onClick={toggleBooker}
-          className="fixed inset-0 -z-1 w-full h-full"
-        ></div>
+        <div className="absolute right-2 top-2">
+          <div
+            onClick={toggleBooker}
+            className="flex gap-2 items-center cursor-pointer"
+          >
+            <div className="flex justify-center items-center bg-background-gray w-[30px] h-[30px]  p-1 rounded-full">
+              <X size={15} strokeWidth={2} className="cursor-pointer" />
+            </div>
+          </div>
+        </div>
         <Cal
           namespace="business-consultation"
           calLink="thecodemaster/business-consultation"
